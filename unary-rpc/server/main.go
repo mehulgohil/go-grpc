2817fe804af93c80e943b4d9fa9556275ad59c37
Mehul Gohil <mehulgohil75@gmail.com>
2023-05-18 16:58:58 +0530

    added unary rpc example

--- unary-rpc/server/main.go
+++ unary-rpc/server/main.go
@@ -17,7 +17,6 @@ func (s server) MakeTransaction(ctx context.Context, in *proto.TransactionReques
 	fmt.Println("Got amount ", in.Amount)
 	fmt.Println("Got from ", in.From)
 	fmt.Println("For ", in.To)
-	// Returning a response of type Transaction Response
 	return &proto.TransactionResponse{Confirmation: true}, nil
 }
 
